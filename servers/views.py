ebdb5b499cc63ed63075af2ea32aa9177fbfbdbd
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-20 23:00:32 +1000

    Forced login

--- servers/views.py
+++ servers/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from servers.models import Server
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def server_list(request):
     servers = Server.objects.all()
     return render(request, 'servers/server_listing.html', {
@@ -9,6 +11,7 @@ def server_list(request):
     })
 
 
+@login_required
 def view_server(request, server_id):
     server = get_object_or_404(Server, pk=server_id)
     return render(request, 'servers/server.html', {
