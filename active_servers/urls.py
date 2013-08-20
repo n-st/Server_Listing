2d58d92f6baccac7bfa5919eededf93c796e5a11
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-20 21:14:41 +1000

    Added a basic server listing

--- active_servers/urls.py
+++ active_servers/urls.py
@@ -9,8 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'active_servers.views.home', name='home'),
     # url(r'^active_servers/', include('active_servers.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', 'servers.views.server_list'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- servers/views.py
+++ servers/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+from servers.models import Server
+
+
+def server_list(request):
+    servers = Server.objects.all()
+    return render(request, 'servers/server_listing.html', {
+        "servers": servers
+    })
