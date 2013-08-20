e2d926179a7d5baebe0f7c2c08264839f310c016
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-20 20:53:54 +1000

    Added servers application

---
+++ servers/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Server'
+        db.create_table(u'servers_server', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('notes', self.gf('django.db.models.fields.TextField')()),
+            ('cost', self.gf('django.db.models.fields.DecimalField')(max_digits=20, decimal_places=2)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'servers', ['Server'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Server'
+        db.delete_table(u'servers_server')
+
+
+    models = {
+        u'servers.server': {
+            'Meta': {'object_name': 'Server'},
+            'cost': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'notes': ('django.db.models.fields.TextField', [], {}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['servers']
--- servers/models.py
+++ servers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Server(models.Model):
+    name = models.CharField(max_length=255)
+    notes = models.TextField()
+
+    cost = models.DecimalField(max_digits=20, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
