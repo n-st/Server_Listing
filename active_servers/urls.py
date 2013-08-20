325a1fc2475ce1446ee733268bb9233beafcfeac
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-20 20:46:37 +1000

    Initial commit

---
+++ active_servers/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'active_servers.views.home', name='home'),
+    # url(r'^active_servers/', include('active_servers.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
