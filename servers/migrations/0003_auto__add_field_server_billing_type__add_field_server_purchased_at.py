1041dfbc000df6a2a09e0d98091ea4c0963da163
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-20 22:09:32 +1000

    Added better timed billing

---
+++ servers/migrations/0003_auto__add_field_server_billing_type__add_field_server_purchased_at.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Server.billing_type'
+        db.add_column(u'servers_server', 'billing_type',
+                      self.gf('django.db.models.fields.CharField')(default='m', max_length=1),
+                      keep_default=False)
+
+        # Adding field 'Server.purchased_at'
+        db.add_column(u'servers_server', 'purchased_at',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Server.billing_type'
+        db.delete_column(u'servers_server', 'billing_type')
+
+        # Deleting field 'Server.purchased_at'
+        db.delete_column(u'servers_server', 'purchased_at')
+
+
+    models = {
+        u'servers.extra_ip': {
+            'Meta': {'object_name': 'Extra_IP'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
+            'server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['servers.Server']"})
+        },
+        u'servers.server': {
+            'Meta': {'object_name': 'Server'},
+            'billing_type': ('django.db.models.fields.CharField', [], {'default': "'m'", 'max_length': '1'}),
+            'cost': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_ip': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'notes': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'purchased_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['servers']
--- servers/models.py
+++ servers/models.py
@@ -4,12 +4,23 @@ from django.core.urlresolvers import reverse
 
 
 class Server(models.Model):
+
+    MONTHLY = 'm'
+    YEARLY = 'y'
+
+    BILLING_CHOICES = (
+        (MONTHLY, 'Monthly'),
+        (YEARLY, 'Yearly'),
+    )
+
     name = models.CharField(max_length=255)
     notes = models.TextField(default='', blank=True)
 
     cost = models.DecimalField(max_digits=20, decimal_places=2)
     main_ip = models.GenericIPAddressField()
 
+    billing_type = models.CharField(max_length=1, choices=BILLING_CHOICES, default=MONTHLY)
+    purchased_at = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
