8c42d3d1e247ec94a3ce3b5a2564c821a1e6bc52
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-20 20:56:53 +1000

    Added django admin support Made notes not required

--- servers/models.py
+++ servers/models.py
@@ -4,9 +4,12 @@ from django.utils import timezone
 
 class Server(models.Model):
     name = models.CharField(max_length=255)
-    notes = models.TextField()
+    notes = models.TextField(default='', blank=True)
 
     cost = models.DecimalField(max_digits=20, decimal_places=2)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
