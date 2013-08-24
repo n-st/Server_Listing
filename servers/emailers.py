e7ace8d691b5564dfeb56eac6c6caed269f1088f
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-24 22:06:08 +1000

    Added email helpers

---
+++ servers/emailers.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+
+def send_mail(subject, message, to=None):
+    if len(settings.ADMINS) is 0:
+        return
+    if to is None:
+        to = [settings.ADMINS[0][1], ]
+
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
