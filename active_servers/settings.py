97d1108b9dadf9c1e02f0d10090d7254445d396d
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-24 22:01:34 +1000

    Added console email for debugging

--- active_servers/settings.py
+++ active_servers/settings.py
@@ -129,6 +129,9 @@ INSTALLED_APPS = (
     'servers',
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'test@example.com'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
