be5516e835ee559af3251b696081c2a3e99990e8
Darayus Nanavati <darayus.contact@gmail.com>
2013-08-26 14:55:23 +1000

    Added logout field

--- active_servers/settings.py
+++ active_servers/settings.py
@@ -106,6 +106,8 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+LOGIN_REDIRECT_URL = '/'
+
 ROOT_URLCONF = 'active_servers.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
--- quick_auth/urls.py
+++ quick_auth/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse_lazy
 
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', {"template_name": 'quick_auth/login.html'}, name='login'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+        "template_name": 'quick_auth/login.html',
+
+    }, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout')
 )
