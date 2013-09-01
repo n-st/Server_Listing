87560a605dc26c78c8da9b099c4d42b9acabf800
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-01 12:51:47 +1000

    Moved to using an ip field

--- servers/models.py
+++ servers/models.py
@@ -317,7 +317,7 @@ class SolusAPI(models.Model):
 
 
 class ResponderAPI(models.Model):
-    api_url = models.CharField(max_length=255)
+    api_url = models.GenericIPAddressField()
     api_key = models.CharField(max_length=255)
     api_port = models.IntegerField(max_length=8)
     server = models.OneToOneField(Server)
