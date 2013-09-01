c3c66b439eb9d73614756881836545fc4a6aecfd
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-01 13:31:56 +1000

    Added a bytes to megabyte conversion

--- servers/responder_api.py
+++ servers/responder_api.py
@@ -2,6 +2,10 @@ import requests
 from requests.exceptions import HTTPError, ConnectionError
 
 
+def bytes_to_megs(value):
+    return float(value) / 1048576.0
+
+
 class ResponderAPI():
     url = ""
     key = ""
@@ -20,6 +24,11 @@ class ResponderAPI():
             "auth_key": self.key,
         }
 
+    def process_mem(self):
+        keys = [key for key in self.response["mem"]]
+        for key in keys:
+            self.response["mem"][key + "_meg"] = bytes_to_megs(self.response["mem"][key])
+
     def send_request(self):
         try:
             request = requests.get(self.url, params=self.params)
@@ -29,4 +38,7 @@ class ResponderAPI():
             self.error = "Could not communicate with the server"
             return self
         self.response = request.json()
+
+        self.process_mem()
+
         return self
